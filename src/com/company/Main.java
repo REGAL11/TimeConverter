876d5154ce04506ddb97d1c480c0879f17b5330b
REGAL11 <cssv34v34v34v34@gmail.com>
2021-11-16 17:57:19 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+        System.out.println(days*24*60*60);
+
+
+    }
+}
